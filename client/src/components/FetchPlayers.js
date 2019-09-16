8b458aa8fce8cbef1fe4844151954bcd6aaab53b
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-09-15 18:37:54 -0700

    player data is getting fetched and displayed

---
+++ client/src/components/FetchPlayers.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PlayerCard from "./PlayerCard";
+
+function FetchPlayers() {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(function(response) {
+        // handle success
+        console.log(response.data);
+        setPlayers(response.data);
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function() {
+        // always executed
+      });
+  }, []);
+
+  return (
+    <div>
+      {players.map(player => {
+        return (
+          <PlayerCard
+            name={player.name}
+            country={player.country}
+            searches={player.searches}
+            key={player.id}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default FetchPlayers;
