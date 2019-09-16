808cda04bc720b1afd445ce546c33aa21ca11d8e
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-09-15 18:50:42 -0700

    Created a custom hook for dark mode kind of...

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import FetchPlayers from "./components/FetchPlayers";
+import { Button } from "semantic-ui-react";
+import useDarkMode from "./hooks/useDarkMode";
 
 function App() {
+  const [darkMode, setDarkMode] = useDarkMode(false);
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  };
   return (
     <div className="App">
+      <Button onClick={toggleMode}>Dark Mode</Button>
       <FetchPlayers />
     </div>
   );
--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -1,22 +1,15 @@
 import React from "react";
-import { Card, Icon, Image } from "semantic-ui-react";
+import { Card } from "semantic-ui-react";
 
 function PlayerCard({ name, country, searches }) {
   return (
     <Card>
-      {/* <Image src="/images/avatar/large/matthew.png" wrapped ui={false} /> */}
       <Card.Content>
         <Card.Header>{name}</Card.Header>
         <Card.Meta>
           <span className="date">{searches}</span>
         </Card.Meta>
         <Card.Description>{country}</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <a>
-          <Icon name="user" />
-          22 Friends
-        </a>
       </Card.Content>
     </Card>
   );
